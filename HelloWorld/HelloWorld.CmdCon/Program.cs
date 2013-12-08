a20b683f14d627ef0ae2e932570ecb97b3798e1e
Sprysloth <sprysloth@gmail.com>
2013-12-08 11:39:01 -0500

    Initial Load

---
+++ HelloWorld/HelloWorld.CmdCon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorld.Domain;
+
+namespace HelloWorld.CmdCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Message msg = new Message();
+            ;
+            for (int i = 0; i < msg.DisplayText.Length;i++ )
+            {
+                Console.WriteLine(msg.DisplayText[i]);
+
+
+            }
+
+
+            foreach (var item in msg.DisplayText)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.Domain/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace HelloWorld.Domain
+{
+    public class Message
+    {
+        public string[] DisplayText { get; set; }
+        public Message()
+        {
+            DisplayText = ConfigurationManager.AppSettings["message"].Split(';');
+        }
+    }
+}
